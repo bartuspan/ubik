c683cfd7a684303b0c8f2d4ebef9c2f34df7193a
Alexander Synaptic <alexander@synapticism.com>
2014-07-10 21:03:10 +0800

    Admin-side tag filter, thumbnails in post list fix

--- ubik/lib/admin.php
+++ ubik/lib/admin.php
@@ -31,17 +31,27 @@ if ( UBIK_ADMIN_VISUAL_EDITOR === false )
 
 // Featured image/post thumbnail column in post list; adapted from http://www.rektproductions.com/display-featured-images-in-admin-post-list/
 function ubik_admin_posts_columns( $defaults ) {
-  $defaults['featured_image'] = __( 'Thumbnails', 'ubik' );
+  $defaults['featured_image'] = __( 'Thumb', 'ubik' );
   return $defaults;
 }
-
 function ubik_admin_posts_custom_columns( $column_name, $id ) {
   if ( $column_name === 'featured_image' )
     echo the_post_thumbnail( array( 60, 60 ) );
 }
+function ubik_admin_posts_columns_style() { // This is a bit of a cheap hack but we're not too concerned about back-end optimization
+  ?><style type="text/css">
+    .column-featured_image {
+      width: 60px;
+    }
+    td.column-featured_image {
+      text-align: center;
+    }
+  </style><?php
+}
 if ( UBIK_ADMIN_POST_LIST_THUMBS ) {
   add_filter( 'manage_posts_columns', 'ubik_admin_posts_columns', 5);
   add_action( 'manage_posts_custom_column', 'ubik_admin_posts_custom_columns', 5, 2);
+  add_action( 'admin_head-edit.php', 'ubik_admin_posts_columns_style' );
 }
 
 
@@ -50,43 +60,33 @@ if ( UBIK_ADMIN_POST_LIST_THUMBS ) {
 
 // Add a tags filter to post list; adapted from https://wordpress.stackexchange.com/questions/578/adding-a-taxonomy-filter-to-admin-list-for-a-custom-post-type
 function ubik_admin_tag_filter() {
-  global $typenow, $wp_query;
+  global $typenow;
 
   if ( $typenow == 'post' ) {
     $taxonomy = 'post_tag';
-    if ( isset( $wp_query->query['term'] ) ) {
-      $term = $wp_query->query['term'];
-    } else {
-      $term = '';
-    }
-    $dropdown_options = array(
+    $term = get_query_var( 'tag_id' );
+    wp_dropdown_categories( array(
       'show_option_all'   => __( 'View all tags', 'ubik' ),
       'hide_empty'        => 1,
       'hierarchical'      => 0,
       'show_count'        => 0,
       'orderby'           => 'name',
-      'name'              => 'tag',
+      'name'              => 'tag_id',
       'taxonomy'          => $taxonomy,
       'selected'          => $term
-    );
-    wp_dropdown_categories( $dropdown_options );
+    ) );
   }
-
 }
-function ubik_admin_tag_convert_id_to_term( $query ) {
-  global $pagenow;
-  $qv = &$query->query_vars;
-  if ( $pagenow == 'edit.php'
-    && isset( $qv['tag'] )
-    && is_numeric( $qv['tag'] )
-  ) {
-    $term = get_term_by( 'id', $qv['tag'], 'post_tag' );
-    $qv['tag'] = $term->slug;
-  }
+
+// This is a workaround for a known WordPress issue: https://core.trac.wordpress.org/ticket/13258
+function ubik_admin_tag_filter_query_vars( $vars ) {
+  if ( is_admin() )
+    $vars[] = "tag_id";
+  return $vars;
 }
 if ( UBIK_ADMIN_TAG_FILTER ) {
+  add_filter( 'query_vars', 'ubik_admin_tag_filter_query_vars' );
   add_action( 'restrict_manage_posts', 'ubik_admin_tag_filter' );
-  add_filter( 'parse_query', 'ubik_admin_tag_convert_id_to_term');
 }
 
 
